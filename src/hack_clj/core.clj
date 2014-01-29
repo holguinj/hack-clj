3b00469f24e4af38dc9bf22d2b7364ad3f989d25
Justin <holguinj@me.com>
2014-01-28 21:35:37 -0800

    I wish my c-instruction lookup function would work, but apparently I recurse too much for Clojure.

--- src/hack_clj/core.clj
+++ src/hack_clj/core.clj
@@ -10,3 +10,32 @@
       (Integer/parseInt)
       (Integer/toString 2)
       (pad 16)))
+
+(defn lookup [cinst] 
+  ({"0" "101010"
+    "1" "111111"
+    "-1" "111010"
+    "D" "001100"
+    "A" "110000"
+    "!D" "001101"
+    "!A" "110001"
+    "-D" "001111"
+    "-A" "110011"
+    "D+1" "011111"
+    "A+1" "110111"
+    "D-1" "001110"
+    "A-1" "110010"
+    "D+A" "000010"
+    "D-A" "010011"
+    "A-D" "000111"
+    "D&A" "000000"
+    "D|A" "010101"
+    "M" (lookup "A")
+    "!M" (lookup "!A")
+    "-M" (lookup "-A")
+    "M+1" (lookup "A+1")
+    "D+M" (lookup "D+A")
+    "D-M" (lookup "D-A")
+    "M-D" (lookup "A-D")
+    "D&M" (lookup "D&A")
+    "D|M" (lookup "D|A")} cinst))
