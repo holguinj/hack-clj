4b46f188c53f828b2ae8e6befa7d9699ce5c8689
Justin <holguinj@me.com>
2014-01-29 20:45:22 -0800

    Added another test for funzies

--- test/hack_clj/core_test.clj
+++ test/hack_clj/core_test.clj
@@ -3,11 +3,13 @@
             [hack-clj.core :refer :all]))
 
 (deftest test-a-instruction?
-  (testing "should return true iff string begins with '@'"
-    (is (= true (a-instruction? "@123")))))
+  (testing "should return true if string begins with '@'"
+    (is (= true (a-instruction? "@123"))))
+  (testing "should return false if non-numbers follow the '@'"
+    (is (= false (a-instruction? "@foo")))))
 
 (deftest test-compile-a-instruction
-  (testing "should always return a 16-digit binary number"
+  (testing "should return a 16-digit binary number"
     (is (= (.length (compile-a-instruction "@123")) 16)))
   (testing "@123 should come back as 123 in binary"
     (is (= 123 (Integer/parseInt (compile-a-instruction "@123") 2)))))
