b46ae355d49fcb98edf576b270d0e1df2e37138c
Justin Holguin <justin.h.holguin@gmail.com>
2014-02-02 00:29:07 -0800

    The refactor is basically complete. '.vm' files are now 'handled', meaning acknowledged and safely ignored.

--- src/hack_clj/core.clj
+++ src/hack_clj/core.clj
@@ -3,21 +3,24 @@
   (:require [hack-clj.asm :refer :all]
             [clojure.java.io :as io]))
 
+(defn fout-name 
+  "Given a filename from the command line, replace '.vm' with '.asm'
+  or replace '.asm' with '.hack'."
+  [^String fin]
+  (cond 
+    (.contains fin ".asm") (clojure.string/replace fin ".asm" ".hack")
+    (.contains fin ".vm") (clojure.string/replace fin ".vm" ".asm")))
+
 (defn -main 
   "Reads a filename from standard input and compiles that file."
   [^String file & args]
-  {:pre [(.contains file ".asm")]}
+  {:pre [(or (.contains file ".asm")
+             (.contains file ".vm"))]}
   (with-open [rdr (io/reader file)]
-    (let [fout (clojure.string/replace file ".asm" ".hack")
+    (let [fout (fout-name file)
           code (->> (line-seq rdr)
                     (map cleanup)
                     (filter (complement clojure.string/blank?)))]
-      (println "Making initial pass on" file "to scan for variables and jump targets.")
-      (parse-vars code)
       (println "Compiling" file "->" fout)
-      (spit fout 
-        (->> code
-             (filter (complement target?))
-             (map hack-compile)
-             (clojure.string/join "\n")))))
+      (if (.contains file ".asm") (compile-asm code fout))))
   (println "Done!"))
