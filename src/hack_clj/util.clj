74a2e39f2e3a83ce654641fba5a3b90b4698ce4e
Justin <holguinj@me.com>
2014-02-02 18:25:47 -0800

    Added utility function re-contains?

--- src/hack_clj/util.clj
+++ src/hack_clj/util.clj
@@ -1,5 +1,9 @@
 (ns hack-clj.util)
 
+(defn re-contains? [^String s re]
+  "Returns true if the string contains the pattern."
+  (false? (nil? (re-find re s))))
+
 (defn pad [^String s n]
   (if (<= n (.length s))
           s
