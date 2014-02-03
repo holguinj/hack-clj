870a05dea1e4f1102037c6f844852dbf6652f95e
Justin <holguinj@me.com>
2014-02-02 21:03:26 -0800

    working on getting push and arithmetic working

--- src/hack_clj/regex.clj
+++ src/hack_clj/regex.clj
@@ -1,6 +1,25 @@
 (ns hack-clj.regex)
 
-(def c-arithmetic #"^add|^sub|^neg|^eq|^gt|^lt|^and|^or|^not")
+(def c-add #"^add")
+
+(def c-sub #"^sub")
+
+(def c-neg #"^neg")
+
+(def c-eq #"^eq")
+
+(def c-gt #"^gt")
+
+(def c-lt #"^lt")
+
+(def c-and #"^and")
+
+(def c-or #"^or")
+
+(def c-not #"^not")
+
+(def c-arithmetic (clojure.string/join "|" [c-add c-sub c-neg c-eq c-gt c-lt c-and c-or c-not]))
+;(def c-arithmetic #"^add|^sub|^neg|^eq|^gt|^lt|^and|^or|^not")
 
 (def c-push #"^push")
 
--- src/hack_clj/vm.clj
+++ src/hack_clj/vm.clj
@@ -1,12 +1,13 @@
 (ns hack-clj.vm
-  (:require [hack-clj.vm-lookup :refer :all]
+  (:require [hack-clj.vm-lookup :as lookup]
             [hack-clj.util :refer :all]
             [hack-clj.regex :as re]))
 
-(defn command-type [^String vm]
+(defn command-type 
   "Given a command in vm language, returns the type of command:
   :c-arithmetic, :c-push, :c-pop, :c-label, :c-goto, :c-if, :c-function,
   :c-return, or :c-call"
+  [^String vm]
   (cond
     (re-contains? vm re/c-arithmetic) :c-arithmetic
     (re-contains? vm re/c-push) :c-push
@@ -15,3 +16,17 @@
     (re-contains? vm re/c-function) :c-function
     (re-contains? vm re/c-return) :c-return
     (re-contains? vm re/c-call) :c-call))
+
+(defn compile-arithmetic [^String vm]
+  {:pre [(= :c-arithmetic (command-type vm))]}
+  (cond
+    (re-contains? vm c-add) lookup/add 
+    (re-contains? vm c-sub) lookup/sub))
+
+(comment (defn c-compile 
+  "Given a line of pure vm code, return a seq of assembly commands"
+  [^String vm]
+  (case (command-type vm)
+    :c-arithmetic (compile-arithmetic vm)
+    ) 
+  ))
