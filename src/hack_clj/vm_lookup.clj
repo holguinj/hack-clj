4436827d2e1b7b32eb209290d4786801edb840a1
Justin <holguinj@me.com>
2014-02-04 19:47:41 -0800

    Added a couple new stack operations

--- src/hack_clj/vm_lookup.clj
+++ src/hack_clj/vm_lookup.clj
@@ -2,44 +2,88 @@
 
 (def init 
   '("//Init"
-   "@256"
-   "D=A"
-   "@SP"
-   "M=D"))
+    "@256"
+    "D=A"
+    "@SP"
+    "M=D"))
 
 (def push
   '("//push above @value to the stack"
-   "D=A"
-   "@SP"
-   "A=M"
-   "M=D"
-   "@SP"
-   "M=M+1"))
+    "D=A"
+    "@SP"
+    "A=M"
+    "M=D"
+    "@SP"
+    "M=M+1"))
 
 (def add
   '("//add"
-   "@SP"
-   "M=M-1"
-   "A=M"
-   "D=M"
-   "M=0"
-   "@SP"
-   "M=M-1"
-   "A=M"
-   "M=M+D"
-   "@SP"
-   "M=M+1"))
+    "@SP"
+    "M=M-1"
+    "A=M"
+    "D=M"
+    "M=0"
+    "@SP"
+    "M=M-1"
+    "A=M"
+    "M=M+D"
+    "@SP"
+    "M=M+1"))
 
 (def sub
   '("//sub"
-   "@SP"
-   "M=M-1"
-   "A=M"
-   "D=M"
-   "M=0"
-   "@SP"
-   "M=M-1"
-   "A=M"
-   "M=M-D"
-   "@SP"
-   "M=M+1"))
+    "@SP"
+    "M=M-1"
+    "A=M"
+    "D=M"
+    "M=0"
+    "@SP"
+    "M=M-1"
+    "A=M"
+    "M=M-D"
+    "@SP"
+    "M=M+1"))
+
+(def and
+  '("//and"
+    "@SP"
+    "M=M-1"
+    "A=M"
+    "D=M"
+    "@SP"
+    "M=M-1"
+    "A=M"
+    "M=M&D"
+    "@SP"
+    "M=M+1"))
+
+(def neg
+  '("//neg"
+    "M=M-1"
+    "A=M"
+    "M=-M"
+    "@SP"
+    "M=M+1"))
+
+(def not
+  '("//not"
+    "@SP"
+    "M=M-1"
+    "A=M"
+    "M=!M"
+    "@SP"
+    "M=M+1"))
+
+(def or
+  '("@SP"
+    "M=M-1"
+    "A=M"
+    "D=M"
+    "@SP"
+    "M=M-1"
+    "A=M"
+    "M=M|D"
+    "@SP"
+    "M=M+1"))
+
+
