12a003b36883fc5dc941b7bf8bfa4b39dd542a95
Justin <holguinj@me.com>
2014-04-02 09:46:44 -0700

    The lexer seems to work OK

---
+++ src/hack_clj/jack_lexer.clj
@@ -0,0 +1,69 @@
+(ns hack-clj.jack-lexer
+  (:require [clojure.data.xml :as xml]
+            [clojure.string :as s]))
+
+(defn- escape [s]
+  (-> s
+      (s/replace "&" "&amp;")  
+      (s/replace "<" "&lt;")
+      (s/replace ">" "&gt;")))
+
+(defn- strip-comments
+  "Takes a string and deletes all comments.
+  Comments are defined as //...\n or /*...*/"
+  [s]
+  (-> s
+      (s/replace #"\/\/.*[$\n\r]+" "")
+      (s/replace #"(?s)\/\*.*\*\/" "")))
+
+(def keywords
+  #{"class" "constructor" "function" "method" "field" "static"
+    "var" "int" "char" "boolean" "void" "true" "false" "null"
+    "this" "let" "do" "if" "else" "while" "return"})
+
+(def symbols
+  #{"{" "}" "(" ")" "[" "]" "." "," ";" "+" "_" "*" "/" "&" "|" "\""
+    "<" ">" "=" "-"})
+
+(defn jack-keyword? [w]
+  (boolean (keywords w)))
+
+(defn jack-symbol? [c]
+  (boolean (symbols c)))
+
+(defn jack-integer? [w]
+  (boolean (re-matches #"^[0-9]{1,5}$" w)))
+
+(defn jack-string? [w]
+  (boolean (re-matches #"^[^\"\n]*$" w)))
+
+(defn jack-identifier? [w]
+  (boolean (re-matches #"^[0-9a-zA-Z_]+$" w)))
+
+(defn token-type [w]
+  (cond
+    (jack-keyword? w) "keyword"
+    (jack-symbol? w) "symbol"
+    (jack-integer? w) "integerConstant"
+    (jack-identifier? w) "identifier"
+    (jack-string? w) "stringConstant"))
+
+(defn split-input [s]
+  (re-seq
+    #"[\{\}\(\)\[\]\.\,;\+_\*\/\&\|<>\=\-\"]|[0-9a-zA-Z_]+"
+    s))
+
+
+(defn xml-string [w]
+  (let [tag (identify w)]
+    (str "<" tag "> "
+         (escape w)
+         " </" tag ">")))
+
+(defn tokenize [filename]
+  (let [fin (-> filename (slurp) (strip-comments) (split-input))
+        fout-name (s/replace filename ".jack" ".xml")]
+    (spit fout-name 
+          (str "<tokens>\n"
+               (s/join "\n" (map xml-string fin))
+               "\n</tokens>\n"))))
