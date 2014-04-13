2f799ec65b6bf032d1c660d13960e4ec9d743808
Justin Holguin <justin.h.holguin@gmail.com>
2014-04-12 17:50:44 -0700

    WIP: reset me

--- src/hack_clj/jack_analyzer.clj
+++ src/hack_clj/jack_analyzer.clj
@@ -28,4 +28,26 @@
        (second)
        (keyword)))
 
+(defn fresh-state []
+  "Returns a map with blank atomic lists to be used
+  as stacks during recursive descent."
+  {:paren (atom ())
+   :brace (atom ())})
+
+(defn dig-in [state punctuation tag-type]
+  "Given a state map, punctuation (:paren or :brace)
+  and the tag-type (e.g., classDec), push the tag onto
+  the appropriate stack and return the opening XML for
+  the tag."
+  (swap! (state punctuation) conj tag-type)
+  (str "<" tag-type ">"))
+
+(defn back-out [state punctuation]
+  "The inverse of dig-in. Takes a state map and punctuation
+  (presumably closing) and pops the tag from the corresponding
+  stack. Returns the closing XML for the tag."
+  (let [tag-type (first @(state punctuation))]
+    (swap! (state punctuation) rest)
+    (str "</" tag-type ">")))
+
 (defmulti compile-nonterminal token-value)
